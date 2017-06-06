56864b646068c30e1514bc67e4b281e82e7a680d
Jeremy Low <jeremylow@gmail.com>
2017-06-06 16:21:02 -0400

    update css & favicon & nginx /media/ config

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -8,6 +8,8 @@ DEBUG = True
 SECRET_KEY = '00wh+stv9wh8&w#*+p#6487$(!d!_^8q=u*!+%8)!7gsc=sfe)'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+ALLOWED_HOSTS = ['*']
+
 
 try:
     from .local import *  # noqa
