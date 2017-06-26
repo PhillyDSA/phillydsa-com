fe211beb1b50de0765efdc3dec947e3f40d30ca6
Jeremy Low <jeremylow@gmail.com>
2017-06-25 21:03:04 -0400

    add HOST_NAME param to config/settings/base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -22,6 +22,7 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'https://phillydsa.com'
+HOST_NAME = 'phillydsa.com'
 WAGTAIL_SITE_NAME = "PhillyDSA"
 
 # Application definition
